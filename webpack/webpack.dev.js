79bacb45181e5a1241a821101843d027fe8f51e1
Peter Goedeke <peterbgoedeke@gmail.com>
2021-11-05 11:31:41 +1300

    feat: initial commit

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
+
+const dev = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        open: true,
+    },
+}
+
+module.exports = merge(common, dev)
