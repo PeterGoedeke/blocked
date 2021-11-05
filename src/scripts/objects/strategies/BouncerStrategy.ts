f93116668659162d597cc482314b673674c8d250
Peter Goedeke <peterbgoedeke@gmail.com>
2021-11-05 22:05:36 +1300

    feat: bouncer block

---
+++ src/scripts/objects/strategies/BouncerStrategy.ts
@@ -0,0 +1,14 @@
+import BlockStrategy from './BlockStrategy'
+import Player from '../Player'
+
+export default class BouncerStrategy extends BlockStrategy {
+    handle(player: Player) {
+        const d = this.gridCoordinates.subtract(player.gridCoordinates)
+        d.rotate(Math.PI / 2)
+        player.setGridDirection(d)
+    }
+
+    get gridCoordinates() {
+        return this.block.gridCoordinates
+    }
+}
