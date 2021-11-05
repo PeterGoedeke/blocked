c5fd4f6bf4451ea92220fa16ab30768668fd24c1
Peter Goedeke <peterbgoedeke@gmail.com>
2021-11-05 22:03:41 +1300

    feat: grid helper functions

---
+++ src/scripts/objects/GridManager.ts
@@ -0,0 +1,9 @@
+export const cellSize = 60
+
+export const gridCoordinatesToPhaser = (vec: Phaser.Math.Vector2) => {
+    return new Phaser.Math.Vector2(vec.x * 60, vec.y * 60)
+}
+
+export const phaserCoordinatesToGrid = (vec: Phaser.Math.Vector2) => {
+    return new Phaser.Math.Vector2(Math.round(vec.x / cellSize), Math.round(vec.y / cellSize))
+}
