92cf79e77d68b9289015092eaa40fdc816ef5c21
Peter Goedeke <peterbgoedeke@gmail.com>
2021-11-11 20:09:52 +1300

    feat: levelscene dynamically loads level

--- src/scripts/scenes/LevelScene.ts
+++ src/scripts/scenes/LevelScene.ts
@@ -1,61 +1,125 @@
 import Block from '../objects/Block'
-import BlockFactory from '../objects/BlockFactory'
+import BlockFactory, { isPickup } from '../objects/BlockFactory'
 import { cellSize } from '../objects/GridManager'
 import Player from '../objects/Player'
+import '../objects/LevelManager'
+import BlockStrategy from '../objects/strategies/BlockStrategy'
+
+interface Level {
+    width: number
+    height: number
+    playerStart: {
+        x: number
+        y: number
+    }
+    blocks: {
+        x: number
+        y: number
+        type: string
+        linkCode: string
+    }[]
+}
+
+interface Links {
+    [key: string]: BlockStrategy[]
+}
 
 export default class LevelScene extends Phaser.Scene {
     player!: Player
     blocks!: Phaser.Physics.Arcade.StaticGroup
     pickups!: Phaser.Physics.Arcade.Group
     entities!: Phaser.Physics.Arcade.Group
+    level!: Level
+    blockFactory: BlockFactory
+    links: Links
 
     constructor() {
         super({ key: 'LevelScene' })
+
+        this.blockFactory = BlockFactory.getInstance()
+        this.links = {}
+    }
+
+    init(level: Level) {
+        this.level = level
+    }
+
+    addBlock(type: string, gridX: number, gridY: number) {
+        const block = this.blockFactory.createBlockFromCode(
+            type,
+            this,
+            cellSize * gridX,
+            cellSize * gridY
+        )
+
+        if (isPickup(type)) {
+            this.pickups.add(block)
+        } else {
+            this.blocks.add(block)
+        }
+
+        return block
+    }
+
+    setPlayer(gridX: number, gridY: number) {
+        this.player = new Player(this, gridX * cellSize, gridY * cellSize)
+        this.entities.add(this.player)
     }
 
     create() {
-        const factory = BlockFactory.getInstance()
-
         this.blocks = this.physics.add.staticGroup()
         this.pickups = this.physics.add.group()
         this.entities = this.physics.add.group({
-            collideWorldBounds: true
+            collideWorldBounds: false
         })
 
-        const a = factory.createBlockFromCode('b', this, cellSize * 1, cellSize * 1)
-        const b = factory.createBlockFromCode('b', this, cellSize * 8, cellSize * 8)
+        this.level.blocks.forEach(blockRaw => {
+            const block = this.addBlock(blockRaw.type, blockRaw.x, blockRaw.y)
+            if (!this.links[blockRaw.linkCode]) {
+                this.links[blockRaw.linkCode] = []
+            }
 
-        const c = factory.createBlockFromCode('i', this, cellSize * 10, cellSize * 8)
-        const d = factory.createBlockFromCode('h', this, cellSize * 3, cellSize * 5)
+            this.links[blockRaw.linkCode].forEach(strategy => {
+                strategy.link(block.strategy)
+                block.strategy.link(strategy)
+            })
 
-        this.blocks.addMultiple([
-            factory.createBlockFromCode('e', this, 0, cellSize * 4),
-            factory.createBlockFromCode('a', this, cellSize * 1, cellSize * 5),
-            factory.createBlockFromCode('e', this, cellSize * 1, cellSize * 2),
-            factory.createBlockFromCode('e', this, cellSize * 5, cellSize * 3),
-            factory.createBlockFromCode('a', this, cellSize * 2, cellSize * 5),
-            factory.createBlockFromCode('a', this, cellSize * 2, cellSize * 4),
-            // factory.createBlockFromCode('g', this, cellSize * 3, cellSize * 5),
-            factory.createBlockFromCode('d', this, cellSize * 7, cellSize * 4),
-            factory.createBlockFromCode('a', this, cellSize * 4, cellSize * 5),
-            factory.createBlockFromCode('a', this, cellSize * 5, cellSize * 5),
-            factory.createBlockFromCode('a', this, cellSize * 6, cellSize * 1),
-            factory.createBlockFromCode('a', this, cellSize * 4, cellSize * 1),
-            factory.createBlockFromCode('a', this, cellSize * 0, cellSize * 0),
-            factory.createBlockFromCode('a', this, cellSize * 11, cellSize * 8),
-            a,
-            b,
-            d
-        ])
-        this.pickups.add(c)
+            this.links[blockRaw.linkCode].push(block.strategy)
+        })
 
-        a.strategy.link(b.strategy)
-        b.strategy.link(a.strategy)
+        this.setPlayer(this.level.playerStart.x, this.level.playerStart.y)
 
-        c.strategy.link(d.strategy)
+        // const a = factory.createBlockFromCode('b', this, cellSize * 1, cellSize * 1)
+        // const b = factory.createBlockFromCode('b', this, cellSize * 8, cellSize * 8)
 
-        this.player = new Player(this, 3 * cellSize, 1 * cellSize)
-        this.entities.add(this.player)
+        // const c = factory.createBlockFromCode('i', this, cellSize * 10, cellSize * 8)
+        // const d = factory.createBlockFromCode('h', this, cellSize * 3, cellSize * 5)
+
+        // this.blocks.addMultiple([
+        //     factory.createBlockFromCode('e', this, 0, cellSize * 4),
+        //     factory.createBlockFromCode('a', this, cellSize * 1, cellSize * 5),
+        //     factory.createBlockFromCode('e', this, cellSize * 1, cellSize * 2),
+        //     factory.createBlockFromCode('e', this, cellSize * 5, cellSize * 3),
+        //     factory.createBlockFromCode('a', this, cellSize * 2, cellSize * 5),
+        //     factory.createBlockFromCode('a', this, cellSize * 2, cellSize * 4),
+        //     // factory.createBlockFromCode('g', this, cellSize * 3, cellSize * 5),
+        //     factory.createBlockFromCode('d', this, cellSize * 7, cellSize * 4),
+        //     factory.createBlockFromCode('a', this, cellSize * 4, cellSize * 5),
+        //     factory.createBlockFromCode('a', this, cellSize * 5, cellSize * 5),
+        //     factory.createBlockFromCode('a', this, cellSize * 6, cellSize * 1),
+        //     factory.createBlockFromCode('a', this, cellSize * 4, cellSize * 1),
+        //     factory.createBlockFromCode('a', this, cellSize * 0, cellSize * 0),
+        //     factory.createBlockFromCode('a', this, cellSize * 11, cellSize * 8),
+        //     a,
+        //     b,
+        //     d
+        // ])
+        // this.pickups.add(c)
+
+        // a.strategy.link(b.strategy)
+        // b.strategy.link(a.strategy)
+
+        // c.strategy.link(d.strategy)
 
         this.physics.add.collider(this.player, this.blocks, (p, b) => {
             const block = b as Block
@@ -93,9 +157,24 @@ export default class LevelScene extends Phaser.Scene {
                 this.player.setGridDirection(new Phaser.Math.Vector2(0, 1))
             }
         })
+
+        this.input.keyboard.on('keydown', (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                this.scene.launch('MenuOverlayScene', this.scene)
+                this.scene.pause()
+            }
+        })
     }
 
     update() {
-        // test
+        if (
+            !this.physics.world.bounds.contains(this.player.x, this.player.y) ||
+            !this.physics.world.bounds.contains(
+                this.player.x + this.player.displayWidth,
+                this.player.y + this.player.displayHeight
+            )
+        ) {
+            this.scene.restart()
+        }
     }
 }
