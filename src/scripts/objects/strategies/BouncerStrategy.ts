b9812588f9460fcf4728254bb2f8724afd72a367
Peter Goedeke <peterbgoedeke@gmail.com>
2021-11-12 22:48:55 +1300

    feat: fixed bug where bouncer would place player slightly off grid

--- src/scripts/objects/strategies/BouncerStrategy.ts
+++ src/scripts/objects/strategies/BouncerStrategy.ts
@@ -1,11 +1,17 @@
 import BlockStrategy from './BlockStrategy'
 import Player from '../Player'
+import { cellSize } from '../GridManager'
 
 export default class BouncerStrategy extends BlockStrategy {
     handle(player: Player) {
         const d = this.gridCoordinates.subtract(player.gridCoordinates)
         d.rotate(-Math.PI / 2)
         player.setGridDirection(d)
+
+        setTimeout(() => {
+            player.setX(Math.round(player.x / cellSize) * cellSize)
+            player.setY(Math.round(player.y / cellSize) * cellSize)
+        }, 0)
     }
 
     get gridCoordinates() {
