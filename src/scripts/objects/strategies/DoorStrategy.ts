4cc517533ebd6a0741d1b21d8e0172ab304286cb
Peter Goedeke <peterbgoedeke@gmail.com>
2021-11-17 15:15:58 +1300

    feat: lock and key animations

--- src/scripts/objects/strategies/DoorStrategy.ts
+++ src/scripts/objects/strategies/DoorStrategy.ts
@@ -8,6 +8,20 @@ export default class DoorStrategy extends BlockStrategy {
 
     unlock() {
         this.block.strategy = new PlatformStrategy(this.block)
-        this.block.setTint(0xc98fc8)
+        this.block.setTexture('lock-open')
+        this.block.scene.tweens.add({
+            targets: this.block,
+            scaleX: 0.65,
+            scaleY: 0.65,
+            duration: 100,
+            ease: 'Power2',
+            yoyo: true
+        })
+        this.block.scene.tweens.add({
+            targets: this.block,
+            alpha: 0.6,
+            duration: 100,
+            ease: 'Power2'
+        })
     }
 }
--- src/scripts/objects/strategies/KeyStrategy.ts
+++ src/scripts/objects/strategies/KeyStrategy.ts
@@ -6,25 +6,32 @@ import Block from '../Block'
 export default class KeyStrategy extends BlockStrategy {
     door?: DoorStrategy
 
+    constructor(block: Block, texture?: string) {
+        super(block, texture)
+
+        this.block.scene.tweens.add({
+            targets: this.block,
+            scaleX: 0.65,
+            scaleY: 0.65,
+            alpha: 1,
+            ease: Phaser.Math.Easing.Sine.InOut,
+            repeat: -1,
+            yoyo: true,
+            duration: 800
+        })
+    }
+
     handle(player: Player) {
         if (!this.door) {
             throw Error(`Key at ${this.block.gridCoordinates} is not linked`)
         }
         this.door.unlock()
-        this.block.scene.tweens.add({
-            targets: this.door.block,
-            scaleX: 0.65,
-            scaleY: 0.65,
-            alpha: 0.8,
-            duration: 100,
-            ease: 'Power2',
-            yoyo: true
-        })
         this.block.destroy()
         this.block.setVisible(false)
     }
 
     override link(strategy: DoorStrategy) {
+        this.block.setAlpha(0.7)
         this.door = strategy
     }
 
